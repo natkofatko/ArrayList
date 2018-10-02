d87de8cc174ae74c6a887fecbf696b4f0ab55523
natal <natalialal2325@gmail.com>
2018-10-02 14:33:18 -0400

    Added Complex Number

--- Week5thClasses/src/Classes/Quadratic.java
+++ Week5thClasses/src/Classes/Quadratic.java
@@ -1,92 +1,205 @@
 package Classes;
 
-
-
+/**
+ * 
+ * @author natalia
+ *
+ */
 public class Quadratic {
-	
-	private double a; 
-	private double b; 
-	private double c; 
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
+
+	private double a;
+	private double b;
+	private double c;
+
 	public String toString() {
 		return "Quadratic [a=" + a + ", b=" + b + ", c=" + c + "]";
 	}
-	public Quadratic()
-	{
-		
-	}
-	public Quadratic(double a, double b, double c)
-	{
-		this.a=a;
-		this.b=b;
-		this.c=c; 
-		
-	}
-	public double calculateDiscriminant()
-	{
-		double disc = b*b-(4*a*c); 
-		return disc;
-		}
-	public double calculateFirstRoot()
-	{
-		double firstRoot= (-b-Math.sqrt(b*b-(4*a*c)))/2.0*a; 
-		return firstRoot; 
-	}
-	public double calculateSecondRoot()
-	{
-		double secondRoot= (-b+Math.sqrt(b*b-(4*a*c)))/2.0*a; 
-		return secondRoot; 
-	}
-	
-	public boolean checkForRealRoot()
-	{
-		if(b*b-(4*a*c)<0)
-		{return false;}
-		else return true; 
-			
-		}
+
 	/**
+	 * Default constructor which makes parabole y=x^2;
+	 */
+	public Quadratic() {
+		this.a = 1;
+		this.b = 0;
+		this.c = 0;
+	}
+
+	/**
+	 * Constructor accepting @param a @param b @param c
+	 */
+	public Quadratic(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+	}
+
+	/**
+	 * get the a value
+	 * 
 	 * @return the a
 	 */
 	public double getA() {
 		return a;
 	}
+
 	/**
-	 * @param a the a to set
+	 * @param a
+	 *            Set the a value
 	 */
 	public void setA(double a) {
 		this.a = a;
 	}
+
 	/**
+	 * Get the b value
+	 * 
 	 * @return the b
 	 */
 	public double getB() {
 		return b;
 	}
+
 	/**
-	 * @param b the b to set
+	 * @param b
+	 *            Set the b value
 	 */
 	public void setB(double b) {
 		this.b = b;
 	}
+
 	/**
+	 * Get the c value
+	 * 
 	 * @return the c
+	 * 
 	 */
 	public double getC() {
 		return c;
 	}
+
 	/**
-	 * @param c the c to set
+	 * @param c
+	 *            Set the c value
 	 */
 	public void setC(double c) {
 		this.c = c;
 	}
 
-	
-	
+	/**
+	 * Compute the descriminant value
+	 * 
+	 * @return descriminant
+	 */
+	public double getDiscriminant() {
+		double discriminant = Math.pow(this.b, 2) - (4 * this.a * this.c);
+		return discriminant;
+	}
+
+	/**
+	 * Check if discriminant is negative or not
+	 * 
+	 * @return true or false
+	 */
+	public boolean hasRealRoot() {
+
+		if (getDiscriminant() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	/**
+	 * Check if discriminant is negative or not
+	 * 
+	 * @return true or false
+	 */
+	public boolean hasComplexRoots() {
+
+		if (getDiscriminant() > 0) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	/**
+	 * Compute the value of real roots
+	 * 
+	 * @return value of real root1 and root2
+	 */
+	public double[] calculatesRealRoots() {
+
+		double root1 = (-b - Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / 2.0 * this.a;
+		double root2 = (-b + Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / 2.0 * this.a;
+
+		return new double[] { root1, root2 };
+
+	}
+
+	/**
+	 * *compute the value of the first derivative of the quadratic at any
+	 * specific point x
+	 * 
+	 * @param x
+	 * @return derivative
+	 */
+	public double slopeAtX(double x) {
+		double derivitive = 2 * this.a * x + this.b;
+		return derivitive;
+	}
+
+	public double calculateOneRoot() {
+		double root = -b / (2 * a);
+		return root;
+	}
+
+	/**
+	 * If discriminant is a negative number then the quadratic equation will
+	 * have no Real solutions. Instead there will be two Complex number. we can
+	 * divide formula for computing roots into two parts:
+	 */
+	public void calculateComplexRoots() {
+		double discriminant = Math.pow(this.b, 2) - (4 * this.a * this.c);
+		double realNumber = -b / (2 * this.a);
+		double complexNumber = Math.sqrt(-discriminant) / (2 * this.a);
+
+		// using printf method to get a specified standard format of complex
+		// numbers
+		System.out.printf("root1 = %.4f+%.3fi", realNumber, complexNumber);
+		System.out.println(" ");
+		System.out.printf("root2 = %.4f-%.3fi", realNumber, complexNumber);
+		System.out.println(" ");
+
+	}
+
+	/**
+	 * Calculate the vertex of the parabola (x1,y1)
+	 * 
+	 * @return vertex
+	 */
+	public double[] getVertex() {
+
+		double x1 = -b / 2 * this.a;
+		double y1 = this.a * Math.pow(x1, 2) + this.b * x1 + this.c;
+
+		return new double[] { x1, y1 };
+
+	}
+
+	/**
+	 * 
+	 * @param (x,y)
+	 * @return
+	 */
+	public String evaluatePolonymialAtX(double x) {
+
+		double[] xy = { x, this.a * Math.pow(x, 2) + this.b * x + this.c };
+
+		return "x: " + xy[0] + "  " + "y:" + xy[1];
+	}
 
 }
--- Week5thClasses/src/Classes/QuadraticDriver.java
+++ Week5thClasses/src/Classes/QuadraticDriver.java
@@ -4,16 +4,32 @@ public class QuadraticDriver {
 
 	public static void main(String[] args) {
 
-		
-		Quadratic calc = new Quadratic(1,4 , 4
-				); 
-		
-		System.out.println(calc.calculateDiscriminant());
-		System.out.println(calc.checkForRealRoot());
-		System.out.println(calc.calculateSecondRoot());
-		System.out.println(calc.calculateFirstRoot());
-		
-		
-	}
+		Quadratic quadratic = new Quadratic(1, -2, -8);
+		double disc = quadratic.getDiscriminant();
 
+		// condition for real and different roots
+		if (disc > 0) {
+			double rootResult[] = quadratic.calculatesRealRoots();
+			System.out.println("root 1: " + rootResult[0] + " root 2: " + rootResult[1]);
+
+			// Condition for real and only one root
+		} else if (disc == 0) {
+			System.out.println(quadratic.calculateOneRoot());
+			// condition for complex number roots
+		} else if (disc < 0) {
+			quadratic.calculateComplexRoots();
+		}
+
+		double vextor[] = quadratic.getVertex();
+		System.out.println("Vertex: " + vextor[0] + " :" + vextor[1]);
+
+		System.out.println(quadratic.hasComplexRoots());
+
+		System.out.println(quadratic.hasRealRoot());
+
+		System.out.println(quadratic.slopeAtX(2));
+
+		System.out.println(quadratic.evaluatePolonymialAtX(2));
+
+	}
 }
